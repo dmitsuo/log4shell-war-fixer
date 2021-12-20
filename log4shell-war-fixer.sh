171e11162696ed3a17281bcd529bbd1c5a6ee847
Davi <davimss@gmail.com>
2021-12-20 10:40:36 -0300

    Check if zip/unzip is available. Some small fixes on messages.

--- log4shell-war-fixer.sh
+++ log4shell-war-fixer.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-set -x
+#set -x
 
 LOG4J_FILE_PATTERN=log4j-core-
 JAR_FOLDER=WEB-INF/lib
 LOG4J_WAR_FILEPATH=${JAR_FOLDER}/${LOG4J_FILE_PATTERN}
 JNDILOOKUP_CLASS_PATH=org/apache/logging/log4j/core/lookup/JndiLookup.class
+
+if ! [ -x "$(command -v zip)" ]; then
+    echo "ERROR: \"zip\" command not found! You need to install \"zip\" in order to run this program."
+    exit 1
+fi
+
+if ! [ -x "$(command -v unzip)" ]; then
+    echo "ERROR: \"unzip\" command not found! You need to install \"unzip\" in order to run this program."
+    exit 1
+fi
 
 if [ $# -eq 0 ]
 then
@@ -17,7 +27,12 @@ WAR_FILE="$1"
 LOWERCASE_WAR_FILE="${WAR_FILE,,}"
 
 if [[ ! ${LOWERCASE_WAR_FILE} =~ \.war$ ]]; then
-    echo "ERROR: Filename is not a WAR!"
+    echo "ERROR: \"${WAR_FILE}\" is not a WAR file!"
+    exit 1
+fi
+
+if [ ! -f "${WAR_FILE}" ]; then	
+	echo "ERROR: \"${WAR_FILE}\" not found!"
     exit 1
 fi
 
@@ -32,7 +47,7 @@ if [ $? -eq 0 ]; then
     
     strings "${LOG4J_FILEPATH}" | grep "${JNDILOOKUP_CLASS_PATH}" > /dev/null
     if [ $? -eq 0 ]; then
-        echo "Removing JndiLookup.class from Log4J..."
+        echo "Removing \"JndiLookup.class\" from Log4J..."
         zip -q -d "${LOG4J_FILEPATH}" "${JNDILOOKUP_CLASS_PATH}"
 
         echo "Backing up original WAR file..."
